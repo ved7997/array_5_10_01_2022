8a267b3372dd57182b0def7907fcef9968996dab
ved7997 <ved7997@gmail.com>
2022-01-10 19:18:22 +0300

    Первая попытка

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,6 +3,19 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-String
+        //Задание №1
+
+        int[] array = new int[3];
+        array[0] = 1;
+        array[1] = 2;
+        array[2] = 3;
+
+
+
+        //Задание №2
+        //Задание №3
+        //Задание №4
+
+
     }
 }
