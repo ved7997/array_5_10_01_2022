f3cb51aba0d23ebd4e55b37162301e97b6412e10
ved7997 <ved7997@gmail.com>
2022-01-10 22:37:44 +0300

    Четвёртая попытка

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -15,17 +15,31 @@ public class Main {
         System.out.println();
 
         //Задание №2
-        String b = ", ";
-        System.out.println(arrayInt[0] + b + arrayInt[1] + b + arrayInt[2]);
-        System.out.println(arrayDouble[0] + b + arrayDouble[1] + b + arrayDouble[2]);
-        System.out.println(arrayString[0] + b + arrayString[1] + b + arrayString[2]);
+        for (int i = 0;i < arrayInt.length;i++) {
+            System.out.print(arrayInt[i] + ", ");
+        }
+            System.out.println();
+        for (int i = 0;i < arrayDouble.length;i++) {
+            System.out.print(arrayDouble[i] + ", ");
+        }
+        System.out.println();
+        for (int i = 0;i < arrayString.length;i++) {
+            System.out.print(arrayString[i] + ", ");
+        }
         System.out.println();
 
         //Задание №3
-        String bb = ", ";
-        System.out.println(arrayInt[2] + bb + arrayInt[1] + bb + arrayInt[0]);
-        System.out.println(arrayDouble[2] + bb + arrayDouble[1] + bb + arrayDouble[0]);
-        System.out.println(arrayString[2] + bb + arrayString[1] + bb + arrayString[0]);
+        for (int i = 2;i >= 0;i--) {
+            System.out.print(arrayInt[i] + ", ");
+        }
+        System.out.println();
+        for (int i = 2;i >= 0;i--) {
+            System.out.print(arrayDouble[i] + ", ");
+        }
+        System.out.println();
+        for (int i = 2;i >= 0;i--) {
+            System.out.print(arrayString[i] + ", ");
+        }
         System.out.println();
 
         //Задание №4
