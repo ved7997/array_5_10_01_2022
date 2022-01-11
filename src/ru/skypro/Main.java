4a0f40d8b279e6642a28b36132804c2af556df10
ved7997 <ved7997@gmail.com>
2022-01-11 22:46:27 +0300

    11_01_2022

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //Задание №1
+        //Задание №
 
         int[] arrayInt = new int[]{1, 2, 3};
 
@@ -15,40 +15,65 @@ public class Main {
 
         //Задание №2
         for (int i = 0;i < arrayInt.length;i++) {
-            System.out.print(arrayInt[i] + ", ");
+            if (arrayInt[i] == 3){
+                System.out.print(arrayInt[i]);
+            }else {
+                System.out.print(arrayInt[i] + ", ");
+            }
         }
             System.out.println();
         for (int i = 0;i < arrayDouble.length;i++) {
-            System.out.print(arrayDouble[i] + ", ");
+            if (arrayDouble[i] == 2.023) {
+                System.out.print(arrayDouble[i]);
+            } else {
+                System.out.print(arrayDouble[i] + ", ");
+            }
         }
-        System.out.println();
+            System.out.println();
+
         for (int i = 0;i < arrayString.length;i++) {
-            System.out.print(arrayString[i] + ", ");
+            if (arrayString[i] == "март") {
+                System.out.println(arrayString[i]);
+            }else {
+                System.out.print(arrayString[i] + ", ");
+            }
         }
         System.out.println();
 
         //Задание №3
         for (int i = 2;i >= 0;i--) {
-            System.out.print(arrayInt[i] + ", ");
+            if (arrayInt[i] == 1){
+                System.out.print(arrayInt[i]);
+            }else {
+                System.out.print(arrayInt[i] + ", ");
+            }
         }
         System.out.println();
         for (int i = 2;i >= 0;i--) {
-            System.out.print(arrayDouble[i] + ", ");
+            if (arrayDouble[i] == 2.021) {
+                System.out.print(arrayDouble[i]);
+            } else {
+                System.out.print(arrayDouble[i] + ", ");
+            }
         }
         System.out.println();
         for (int i = 2;i >= 0;i--) {
-            System.out.print(arrayString[i] + ", ");
+            if (arrayString[i] == "январь") {
+                System.out.println(arrayString[i]);
+            }else {
+                System.out.print(arrayString[i] + ", ");
+            }
         }
         System.out.println();
 
         //Задание №4
-        for (int i = 0; i < arrayInt.length; i++)
-            if (arrayInt[i] % 2 > 0){
-            System.out.print(arrayInt[i] +1 + ", ");
-            }else {
+        for (int i = 0; i < arrayInt.length; i++) {
+            if (arrayInt[i] % 2 > 0) {
+                System.out.print(arrayInt[i] + 1 + ", ");
+            } else {
                 System.out.print(arrayInt[i] + ", ");
             }
-
+        }
 
 
     }
